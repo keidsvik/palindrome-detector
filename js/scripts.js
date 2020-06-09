eddb4ab415e962aba02ab427c89a482b7ea73ab3
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-09 15:12:22 -0700

    add initial html and js logic
    Co-authored by: Sara Kane <kane.sarak@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("form#palindromeform").submit(function(event) {
+  event.preventDefault();
+    const userInput = $("#input").val();
+    let inputArray = userInput.split('');
+
+    inputArray.forEach(function(element) {
+      inputArray[0]
+    }
+
+
+// receive word *
+// turn word into an array
+// check first index and last index to see if equal
+// push into blank array
+// if not equal? not palindrome
+// if equal? palindrome
+
+  });
+});
